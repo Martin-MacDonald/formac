f525dd9908dc407e9ecff8cf912c0098f4bca797
Martin-MacDonald <mmacdo54@caledonian.ac.uk>
2022-01-01 19:14:59 +0000

    Init

---
+++ dist/index.esm.js
@@ -0,0 +1,1 @@
+var e=function(){return React.createElement("div",null,"This is a test")};export{e as Test};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const Test = () => {
+  return <div>This is a test</div>;
+};
+
+export { Test };
